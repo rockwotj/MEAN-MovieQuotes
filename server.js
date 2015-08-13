c988ea91343b802a199e2f8d44903d1e929bbd82
Tyler Rockwood <rockwotj@users.noreply.github.com>
2015-08-13 08:42:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(express.static('public'))
 });
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var server = app.listen(port, ip_address, function () {
   console.log('Example app listening at http://localhost:%s', port);
